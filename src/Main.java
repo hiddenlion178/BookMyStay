f182ae58a2e200b7f98d302fcd904a7c1f307b81
hiddenlion178 <hiddenlion17@gmail.com>
2026-03-10 10:10:11 +0530

    UC2 BASIC ROOM TYPES AND STATIC AVAILABILITY

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,71 @@
 abstract class Room {
 
-    protected String roomType;
-    protected int beds;
-    protected int size;
-    protected double price;
+    protected String type;
+    protected int bedCount;
+    protected int area;
+    protected double cost;
 
-    public Room(String roomType, int beds, int size, double price) {
-        this.roomType = roomType;
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
+    public Room(String type, int bedCount, int area, double cost) {
+        this.type = type;
+        this.bedCount = bedCount;
+        this.area = area;
+        this.cost = cost;
     }
 
-    public void displayRoom() {
-        System.out.println(roomType + ":");
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sqft");
-        System.out.println("Price per night: " + price);
+    public void showDetails() {
+        System.out.println("\nRoom Type : " + type);
+        System.out.println("Beds      : " + bedCount);
+        System.out.println("Area      : " + area + " sq ft");
+        System.out.println("Cost      : Rs." + cost + " per night");
     }
-}
 
-class SingleRoom extends Room {
-    public SingleRoom() {
-        super("Single Room", 1, 250, 1500.0);
+    public void showAvailability(int available) {
+        System.out.println("Available Rooms : " + available);
     }
 }
 
-class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super("Double Room", 2, 400, 2500.0);
+class StandardRoom extends Room {
+
+    public StandardRoom() {
+        super("Standard Room", 1, 210, 1300.0);
     }
 }
 
-class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super("Suite Room", 3, 750, 5000.0);
+class FamilyRoom extends Room {
+
+    public FamilyRoom() {
+        super("Family Room", 2, 360, 2400.0);
     }
 }
 
+class LuxurySuite extends Room {
 
-public class BookMyStay {
+    public LuxurySuite() {
+        super("Luxury Suite", 3, 700, 4800.0);
+    }
+}
+
+public class HotelBooking {
+
     public static void main(String[] args) {
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        int standardAvailable = 5;
+        int familyAvailable = 3;
+        int luxuryAvailable = 2;
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        Room r1 = new StandardRoom();
+        Room r2 = new FamilyRoom();
+        Room r3 = new LuxurySuite();
 
-        System.out.println("Hotel Room Initialization\n");
+        System.out.println(" Hotel Room Details ");
 
-        single.displayRoom();
-        System.out.println("Available: " + singleAvailable + "\n");
+        displayRoomInfo(r1, standardAvailable);
+        displayRoomInfo(r2, familyAvailable);
+        displayRoomInfo(r3, luxuryAvailable);
+    }
 
-        doubleRoom.displayRoom();
-        System.out.println("Available: " + doubleAvailable + "\n");
-
-        suite.displayRoom();
-        System.out.println("Available: " + suiteAvailable);
+    public static void displayRoomInfo(Room room, int available) {
+        room.showDetails();
+        room.showAvailability(available);
     }
 }
