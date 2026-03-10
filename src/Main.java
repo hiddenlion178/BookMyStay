ba4163903fd549130a533c0c386c76a1f0120723
hiddenlion178 <hiddenlion17@gmail.com>
2026-03-10 09:14:10 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("   Welcome to Hotel Booking App   ");
+
+        System.out.println("System started successfully!");
+    }
+}
