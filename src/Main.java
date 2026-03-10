3b05b627a87994f507b413cd1d08947b7cba5cf3
hiddenlion178 <hiddenlion17@gmail.com>
2026-03-10 10:19:44 +0530

    UC3 CENTRALIZED ROOM INVENTORY AND MANAGEMENT

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,53 @@
+import java.util.HashMap;
+
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Standard Room", 5);
+        inventory.put("Family Room", 3);
+        inventory.put("Luxury Suite", 2);
+    }
+
+    public int getAvailability(String roomType) {
+        if (inventory.containsKey(roomType)) {
+            return inventory.get(roomType);
+        }
+        return 0;
+    }
+
+    public void updateAvailability(String roomType, int newCount) {
+        if (inventory.containsKey(roomType)) {
+            inventory.put(roomType, newCount);
+        }
+    }
+
+    public void displayInventory() {
+        System.out.println("Current Room Inventory:");
+        for (String room : inventory.keySet()) {
+            System.out.println(room + " : " + inventory.get(room));
+        }
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 
+        RoomInventory inventory = new RoomInventory();
 
-        System.out.println("   Welcome to Hotel Booking App   ");
+        inventory.displayInventory();
 
-        System.out.println("System started successfully!");
+        System.out.println();
+
+        System.out.println("Standard Room Available: " +
+                inventory.getAvailability("Standard Room"));
+
+        inventory.updateAvailability("Standard Room", 4);
+
+        System.out.println("\nAfter Update:");
+        inventory.displayInventory();
     }
 }
